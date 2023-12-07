7716ba4f57ccd112dadd068dfa99398590d02b34
Ade Putra <adeputra.c@gmail.com>
2023-12-07 15:41:28 +0700

    MyWorkManager

---
+++ app/src/main/java/com/example/myworkmanager/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.myworkmanager;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+
+import android.Manifest;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnPeriodicTask, btnCancelTask;
+    private PeriodicWorkRequest periodicWorkRequest;
+    Button btnOneTimeTask;
+    EditText editCity;
+    TextView textStatus;
+    private WorkManager workManager;
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= 33) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+        }
+        btnOneTimeTask = findViewById(R.id.btnOneTimeTask);
+        editCity = findViewById(R.id.editCity);
+        textStatus = findViewById(R.id.textStatus);
+        btnPeriodicTask = findViewById(R.id.btnPeriodicTask);
+        btnCancelTask = findViewById(R.id.btnCancelTask);
+
+        workManager = WorkManager.getInstance(this);
+
+        btnOneTimeTask.setOnClickListener(this);
+        btnPeriodicTask.setOnClickListener(this);
+        btnCancelTask.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.btnOneTimeTask) {
+            startOneTimeTask();
+        } else if (id == R.id.btnPeriodicTask) {
+            startPeriodicTask();
+        } else if (id == R.id.btnCancelTask) {
+            cancelPeriodicTask();
+        }
+    }
+    private void startOneTimeTask() {
+        textStatus.setText(getString(R.string.status));
+
+        Data data = new Data.Builder()
+                .putString(MyWorker.EXTRA_CITY, editCity.getText().toString())
+                .build();
+
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+
+        OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest.Builder(MyWorker.class)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .build();
+
+        workManager.enqueue(oneTimeWorkRequest);
+
+        workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.getId())
+                .observe(MainActivity.this, workInfo -> {
+                    String status = workInfo.getState().name();
+                    textStatus.append("\n"+status);
+                });
+    }
+
+    private void startPeriodicTask() {
+        textStatus.setText(getString(R.string.status));
+        Data data = new Data.Builder()
+                .putString(MyWorker.EXTRA_CITY, editCity.getText().toString())
+                .build();
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+        periodicWorkRequest = new PeriodicWorkRequest.Builder(MyWorker.class, 15, TimeUnit.MINUTES)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .build();
+        workManager.enqueue(periodicWorkRequest);
+        workManager.getWorkInfoByIdLiveData(periodicWorkRequest.getId())
+                .observe(MainActivity.this, workInfo -> {
+                    String status = workInfo.getState().name();
+                    textStatus.append("\n"+status);
+                    btnCancelTask.setEnabled(false);
+                    if (workInfo.getState() == WorkInfo.State.ENQUEUED){
+                        btnCancelTask.setEnabled(true);
+                    }
+                });
+    }
+    private void cancelPeriodicTask() {
+        workManager.cancelWorkById(periodicWorkRequest.getId());
+    }
+}
---
+++ app/src/main/java/com/example/myworkmanager/MyWorker.java
@@ -0,0 +1,103 @@
+package com.example.myworkmanager;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.SyncHttpClient;
+
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+
+import cz.msebera.android.httpclient.entity.mime.Header;
+
+public class MyWorker extends Worker {
+
+    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    private static final String TAG = MyWorker.class.getSimpleName();
+    private static final String APP_ID = "94ae68e197293f897867528a51624b1c";
+    public static final String EXTRA_CITY = "city";
+    private Result resultStatus;
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        String dataCity = getInputData().getString(EXTRA_CITY);
+        return  getCurrentWeather(dataCity);
+    }
+
+    private Result getCurrentWeather(final String city) {
+        Log.d(TAG, "getCurrentWeather: Mulai.....");
+        Looper.prepare();
+        SyncHttpClient client = new SyncHttpClient();
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APP_ID;
+        Log.d(TAG, "getCurrentWeather: " + url);
+        client.post(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
+                String result = new String(responseBody);
+                Log.d(TAG, result);
+                try {
+                    JSONObject responseObject = new JSONObject(result);
+                    String currentWeather = responseObject.getJSONArray("weather").getJSONObject(0).getString("main");
+                    String description = responseObject.getJSONArray("weather").getJSONObject(0).getString("description");
+                    double tempInKelvin = responseObject.getJSONObject("main").getDouble("temp");
+                    double tempInCelsius = tempInKelvin - 273;
+                    String temperature = new DecimalFormat("##.##").format(tempInCelsius);
+                    String title = "Current Weather in "+city;
+                    String message = currentWeather + ", " + description + " with " + temperature + " celsius";
+                    showNotification(title, message);
+                    Log.d(TAG, "onSuccess: Selesai.....");
+                    resultStatus = Result.success();
+                } catch (Exception e) {
+                    showNotification("Get Current Weather Not Success",e.getMessage());
+                    Log.d(TAG, "onSuccess: Gagal.....");
+                    resultStatus = Result.failure();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
+                showNotification("Get Current Weather Failed",error.getMessage());
+                Log.d(TAG, "onFailure: Gagal.....");
+                resultStatus = Result.failure();
+            }
+        });
+        return resultStatus;
+    }
+    private static final int NOTIFICATION_ID = 1;
+    private static final String CHANNEL_ID = "channel_01";
+    private static final String CHANNEL_NAME = "dicoding_channel";
+    private void showNotification(String title, String description) {
+        NotificationManager notificationManager = (NotificationManager)getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                .setSmallIcon(R.drawable.baseline_notifications_24)
+                .setContentTitle(title)
+                .setContentText(description)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            notification.setChannelId(CHANNEL_ID);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+        if (notificationManager != null) {
+            notificationManager.notify(NOTIFICATION_ID, notification.build());
+        }
+    }
+}
